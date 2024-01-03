de392a6f7ab4cb0d6afca5729d6c806b77976e2d
marcojthompson <63751033+marcojthompson@users.noreply.github.com>
2024-01-03 09:53:48 -0800

    Adding database stuff
    moved directory containing database stuff into app directory

---
+++ db/create_tables.sql
@@ -0,0 +1,9 @@
+USE NFL_PLAYER_SALARY_DATA;
+
+CREATE TABLE IF NOT EXISTS player_cap_hits (
+    player_name VARCHAR(50) NOT NULL,
+    team_loc VARCHAR(3) NOT NULL,
+    cap_hit VARCHAR(15) NOT NULL,
+    PRIMARY KEY (player_name, team_loc)
+);
+
