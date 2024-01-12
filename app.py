301805bd6f5bdc91e8a07027482e7ff6777335b2
marcojthompson <63751033+marcojthompson@users.noreply.github.com>
2024-01-11 21:12:18 -0800

    Team Pages
    Team pages now show all players under contract

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import or_, and_
 
 app = Flask(__name__)
 
@@ -7,20 +8,21 @@ app = Flask(__name__)
 uri = ''
 app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 # Defining initial table
-class cap_hit(db.Model):
+class cap_hits(db.Model):
     __tablename__ = 'player_cap_hits'
 
     player_name = db.Column(db.String(255), primary_key = True)
     team_loc = db.Column(db.String(255), primary_key = True)
     cap_hit = db.Column(db.String(255))
 
-# dummy route to see if database and website are properly linked
+# Dummy route to see if database and website are properly linked
 @app.route('/show_data')
 def show_data():
-    data = cap_hit.query.all()
+    data = cap_hits.query.all()
     return render_template('show_data.html', data = data)
 
 # Homepage
@@ -34,10 +36,11 @@ def process_form():
     team = request.form.get('team_choice')
     return redirect(url_for('show_team_page', team_choice = team))
 
-# Displaying team page
+# Querying database and displaying team page
 @app.route('/team/<team_choice>')
 def show_team_page(team_choice):
-    return render_template('team.html', team = team_choice)
+    players = cap_hits.query.filter_by(team_loc = team_choice).order_by(cap_hits.cap_hit.desc()).all()
+    return render_template('team.html', team = team_choice, data = players)
 
 if __name__ == '__main__':
     app.run(debug = True)
